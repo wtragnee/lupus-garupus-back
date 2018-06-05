0cbad0888208c4ed24d739c88d8a7f096397f218
William Tragnée <w.tragnee@gmail.com>
2018-06-05 15:04:15 +0200

    Stream 2

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Service = require('./src/Helper/Service');
-// const RouteLoader = require('./src/Helper/RouteLoader');
+const RouteLoader = require('./src/Helper/RouteLoader');
 const http = require('http');
 const socketIO = require('socket.io');
 
@@ -8,20 +8,15 @@ const app = express();
 const service = new Service({});
 const container = service.load();
 
-// new RouteLoader({ app, container }).load(container.get('config').routes);
+new RouteLoader({ app, container }).load(container.get('config').routes);
 
 const PORT = container.get('config').app.port;
-// app.listen(PORT, () => {
-//   // eslint-disable-next-line
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
 
 const server = http.createServer(app);
 const io = socketIO(server);
 io.on('connection', socket => {
   console.log('New client connected');
 
-  // just like on the client side, we have a socket.on method that takes a callback function
   socket.on('change color', color => {
     console.log('Color Changed to: ', color);
     io.sockets.emit('change color', color);
@@ -32,4 +27,4 @@ io.on('connection', socket => {
   });
 });
 
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
